49441580dca87b487e17a9583a5e3c0e0b3ede10
Lorenzo <lorenzo.romeo.mail@gmail.com>
2022-10-24 22:03:46 +0200

    Reading from file in progress.

---
+++ binaryKnapsack2.1.c
@@ -0,0 +1,304 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DEBUG 0
+
+// print array
+void printa(int a[], size_t size){
+    for(int i = 0; i<size; i++)
+        printf("%2d ",a[i]);
+    printf("\n");
+}
+
+// print matrix
+void printm (size_t sizei, size_t sizej, int a[sizei][sizej]){
+    for(int i=0; i<sizei; i++){
+        for(int j=0; j<sizej; j++){
+            printf("%3d ", a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+/* FARE DOPO ALLOC 
+void alloc(int **t, int **s, int *t_size, int *s_size, int **res_ks, int n, int capacity){
+    *t_size = capacity+1;
+    (*t) = calloc(*t_size, sizeof(int));
+    //s e s size non si possono calcolare 
+
+    
+}
+*/
+
+// Removes the redundant columns
+int minCap(int *weights, int n, int **t, int *size_t, int **s, int *size_s) {
+
+    // Compute capacity
+    int capacity = 0;
+    for(int i=0; i<n; i++) capacity+=weights[i]; 
+    
+    *size_t = capacity+1;
+
+    // Build table
+    int mat[n+1][capacity+1];
+
+    for(int i = 0; i<n+1;i++)
+        for(int j=0;j<capacity+1;j++) mat[i][j] = capacity;
+
+
+    for(int i=0; i<n+1; i++){
+        for(int j=0; j<capacity+1; j++){
+
+            int w = (i == 0)? 0 : weights[i-1];
+            int p = (i == 0)? 0 : 1;
+
+            if(j == 0){
+                mat[i][j] = 0;
+            }else if(i == 0){
+                mat[i][j] = capacity+1;
+            } else if(j-w < 0){ 
+                mat[i][j] = mat[i-1][j];
+            }else if(mat[i-1][j] < mat[i-1][j-w]+w ){
+                mat[i][j] = mat[i-1][j];
+            } else{
+                mat[i][j] = mat[i-1][j-w]+w; 
+            }
+        }
+    }
+
+    // Build result arrays
+    *size_s = 0;
+    for(int i=0; i<capacity+1; i++)
+        if(mat[n][i] <= capacity) (*size_s)++;
+
+    if(DEBUG){
+        printf("s-size: %d\n", *size_s);
+        printf("t-size: %d\n", *size_t);
+    }
+    *t = calloc(*size_t, sizeof(int));
+    *s = calloc(*size_s, sizeof(int));
+    int counter = 0;
+    int s_cont = 0;
+    
+    for(int i=0; i<capacity+1; i++){
+        if(mat[n][i] <= capacity){
+            (*t)[i] = counter++;
+            (*s)[s_cont++] = i;
+        }else{
+            (*t)[i] = -1;
+        }
+    }
+    return capacity+1;
+}
+
+// 0-1 knapsack
+void ks(int *profits, int *weights, int capacity, int n, short showMatrix){
+    
+    // Table creation
+    int mat[n+1][capacity+1];
+
+    for(int i = 0; i<n+1;i++)
+        for(int j=0;j<capacity+1;j++) mat[i][j] = 0;
+
+    for(int i=0; i<n+1; i++){
+        for(int j=0; j<capacity+1; j++){
+            int w = (i == 0)? 0 : weights[i-1];
+            int p = (i == 0)? 0 : profits[i-1];
+
+            if(i == 0 || j == 0) mat[i][j] = 0;
+            else if(j-w<0) {
+                mat[i][j] = mat[i-1][j];
+            }else if(mat[i-1][j] > mat[i-1][j-w]+p){
+                mat[i][j] = mat[i-1][j];
+            }else{
+                mat[i][j] = mat[i-1][j-w]+p; 
+            }
+
+        }
+    }
+
+    // Result search
+    int *res = calloc(n, sizeof(int));
+    int remainingCapacity = capacity;
+    int indexWeight = capacity;
+
+    for(int i=n; i>0; i--){
+        if(mat[i][indexWeight] != mat[i-1][indexWeight]){
+            res[i-1] = 1;
+            remainingCapacity -= weights[i-1];
+            indexWeight = remainingCapacity;      
+        }
+    }
+
+    // Result visualization
+    int maxprofit = 0;
+
+    if(showMatrix){
+        printf("matrix:\n");
+
+        for(int j = 0; j < capacity+1; j++) printf("%3d ", j);
+        printf("\n");
+        for(int j = 0; j < capacity+1; j++) printf("%3s","_");
+        printf("\n");    
+
+        printm(n+1, capacity+1, mat);
+        printf("\n");
+    }
+
+    printf("Result: ");
+    for(int i=0; i<n; i++){
+        printf("(%d)%d%s", res[i], i, (i+1==n)?".\n":", ");
+        maxprofit += res[i]*profits[i];
+
+    }
+    printf("Maximum profits: %d (for %d capacity)\n", maxprofit, capacity);
+}
+
+// 0-1 knapsack with optimized columns
+// Invariant: capacity cannot be greater than the sum of all the weights
+void ks2(int *profits, int *weights, int capacity, int n, short showMatrix){
+
+    // Get column values
+    int *t;
+    int *s;
+    int t_size = 0;
+    int s_size = 0;
+    minCap(weights, n, &t, &t_size, &s, &s_size);
+
+    // Table creation
+    int mat[n+1][s_size];
+
+    for(int i = 0; i<n+1; i++)
+        for(int j=0; j<s_size; j++) mat[i][j] = 0;
+        
+    for(int i = 0; i < n+1; i++){
+        for(int j = 0; j < s_size; j++){
+            int w = (i == 0)? 0 : weights[i-1];
+            int p = (i == 0)? 0 : profits[i-1];
+
+            int clm = s[j];
+            int cfr = (clm-w < 0)? -1 : (t[ clm - w ]<0) ? t[ clm - w -1 ]: t[ clm - w ];
+
+            if(i == 0 || j == 0) mat[i][j] = 0;
+            else if(cfr < 0){
+                mat[i][j] = mat[i-1][j];
+            }else if(mat[i-1][j] > mat[i-1][cfr]+p){
+                mat[i][j] = mat[i-1][j];
+           } else{
+                mat[i][j] = mat[i-1][cfr]+p; 
+            }
+        }
+    }
+
+    int *res = calloc(n, sizeof(int));
+    int remainingCapacity = capacity;
+    int indexWeight = capacity;
+
+    for(int i=n; i>0; i--){
+        if(t[indexWeight]<0){
+            indexWeight--;
+            i++;
+            continue;
+        } 
+        if(mat[i][t[indexWeight]] != mat[i-1][t[indexWeight]]){
+            res[i-1] = 1;
+            remainingCapacity -= weights[i-1];
+            indexWeight = remainingCapacity;      
+        }
+    }
+    
+    // Result visualization
+    int maxprofit = 0;
+
+    if(showMatrix){
+        printf("matrix:\n");
+
+        for(int j = 0; j < s_size; j++) printf("%3d ", s[j]);
+        printf("\n");
+        for(int j = 0; j < s_size; j++) printf("%3s","_");
+        printf("\n");
+
+        for(int j = 0; j < s_size; j++){
+            printf("%3d ", s[j]);
+        }
+        printf("\n");
+
+        printm(n+1, s_size, mat);
+        printf("\n");
+    }
+
+    printf("Result: ");
+    for(int i=0; i<n; i++){
+        printf("(%d)%d%s", res[i], i, (i+1==n)?".\n":", ");
+        maxprofit += res[i]*profits[i];
+    }
+    printf("Maximum profits: %d (for %d capacity)\n", maxprofit, capacity);
+}
+
+//int readVaues(char* filename, int *size, int **weights, int **profits){
+int readVaues(char* filename, int **profits, int **weights, int *size){
+    FILE *fp;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    fp = fopen(filename, "r");
+    int a=0, b=0, c=0;
+
+    // Read items nr
+    int itemsNr = 0;
+    read = getline(&line, &len, fp);
+    sscanf(line, "%d", &itemsNr);
+    printf("items: %d\n", itemsNr);
+    *size = itemsNr;
+
+    *profits = calloc(*size, sizeof(int));
+    *weights = calloc(*size, sizeof(int));
+    printa(*profits, *size);
+
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+    while ((read = getline(&line, &len, fp)) != -1 && itemsNr--) {
+        sscanf(line, "%5d %5d %5d", &a, &b, &c);
+        printf("a.");
+        *profits[a-1] = b;
+        printf(".");
+        *weights[a-1] = c;
+        printf(".\n");
+        printf("%5d %5d %5d\n", a, b, c);
+    }
+
+    //FINIRE QUI
+    if (ferror(fp)) {
+        printf("File error!\n");
+        exit(EXIT_FAILURE);
+    }
+    free(line);
+    fclose(fp);
+
+}
+
+int main(){
+    /*
+    printf("Test1\n");
+    ks( (int[]){1,1,1,1},(int[]){2,3,5,7}, 17, 4, 1);
+    ks2((int[]){1,1,1,1},(int[]){2,3,5,7}, 17, 4, 1);
+    printf("----\nTest2\n");
+    ks( (int[]){1,4,5,7},(int[]){1,3,4,5}, 19, 4, 0);
+    ks2((int[]){1,4,5,7},(int[]){1,3,4,5}, 19, 4, 0);
+    printf("----\nTest3\n");
+    ks( (int[]){1,4,5,7},(int[]){1,3,4,5}, 4, 4, 0);
+    ks2((int[]){1,4,5,7},(int[]){1,3,4,5}, 4, 4, 0);
+    printf("----\nTest4\n");
+    ks( (int[]){5,4,3,2},(int[]){4,3,2,1}, 10, 4, 1);
+    ks2((int[]){5,4,3,2},(int[]){4,3,2,1}, 10, 4, 1);
+    */
+
+    int *profits;
+    int *weights;
+    int size = 0;
+    readVaues("generator/files/1_10_uncorr.in", &profits, &weights, &size);
+    printf("Values: %d\n", size);
+    printa(profits, size);
+    printa(weights, size);
+    return 0;
+}
